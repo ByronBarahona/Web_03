07e801db9662c45a643710e127e8837e98dd0ccf
ByronBarahona <barahonabyron38@gmail.com>
2023-07-09 17:11:04 -0400

    ok

---
+++ Clases/DB_CFG.php
@@ -0,0 +1,8 @@
+<?php
+//Definimos constantes para el llamado en la conexion
+define('host','localhost');
+define('user','root');
+define('pass','');
+define('db','TiendaMM');
+
+?>
---
+++ codigo_sql/insercion_data.sql
@@ -0,0 +1,4 @@
+use TiendaMM;
+insert into perfil (codigo, descripcion) values (1,'Administrador'),(2,'Vendedor');
+
+insert into usuarios values (1,'admin', 'adm123',1,'20230709');
