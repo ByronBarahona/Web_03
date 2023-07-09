b4fe8f9fbba1a27e6344f10756aee82fc98ff48d
ByronBarahona <barahonabyron38@gmail.com>
2023-07-09 17:18:20 -0400

    ok

---
+++ Clases/ValidaUsuario.php
@@ -0,0 +1,37 @@
+<?php
+require ("Clases/Conexion.php");
+session_start();
+Class Autenticar{
+    private $user;
+    private $clave;
+    private $perfil;
+    
+
+    public function __construct($user,$clave, $perfil){
+        $this->user = $user;
+        $this->clave = $clave;
+        $this->perfil = $perfil;
+    }
+
+    public function Validar(){
+        $conexion = new Conexion();
+        $conexion->Conecta();
+        
+        $consulta = "SELECT * FROM usuario WHERE usr_log = '$this->user' AND usr_pass = '$this->clave' AND perfil = '$this->perfil'";
+        
+        $resultado = $conexion->Ejecuta($consulta);
+
+        if ($resultado->num_rows == 1) {
+           
+            $_SESSION["rut"] = $this->rut;
+            header('Location: Panel.php');
+        } else {
+            header('Location: index.php');
+        }
+
+    }
+
+}
+
+
+?>
