36ecb7cf6626eb2030827d74aa3c7725dfd316c3
GABRIEL FELIPE RODRÍGUEZ CORNEJO <g.rodriguez37@alumnos.santotomas.cl>
2023-07-09 18:31:41 -0400

    otra vez aloz

--- Clases/COncexion2.php
+++ Clases/COncexion2.php
@@ -6,13 +6,7 @@ Class Conexion{
     private $pass;
     private $db;
     private $conexion;
-<<<<<<< HEAD
-    
-=======
 
-
-
->>>>>>> e46956a62f9ed784ad3e7ae9d97426a71ded6762
     public function __construct(){//$host, $user, $pass, $db
         require_once "DB_CFG.php";//obtenemos nuestras constantes
         $this->host = host;        
@@ -25,8 +19,10 @@ Class Conexion{
 
     public function Conecta(){
         //Conexion DB
+        
         //POO
         $this->conexion = new mysqli($this->host,$this->user,$this->pass,$this->db);
+
         //POO
         if($this->conexion->connect_errno){
             die("Conexion Error: (". $this->conexion->connect_errno. ") " . $this->conexion->connect_error);
--- Vistas/Login.php
+++ Vistas/Login.php
@@ -1,28 +1,5 @@
 <?php
-// session_start();
-
-// if ($_SERVER["REQUEST_METHOD"] == "POST") {
-//   $username = $_POST["usr_log"];
-//   $password = $_POST["usr_pass"];
-//   $tip_usr = $_POST["perfil"];
-
-//   // Aquí debes realizar la validación de las credenciales
-//   // Comparar con los datos almacenados en la base de datos
-
-//   // if ($username == "usr_log" && $password == "usr_pass" && $tip_usr == "perfil") {
-//   //   $_SESSION["user_type"] = "admin";
-//   //   header("Location: admin.php");
-//   //   exit;
-//   // } elseif ($username == "vendedor" && $password == "vendedor123") {
-//   //   $_SESSION["user_type"] = "vendedor";
-//   //   $_SESSION["vendedor_nombre"] = "Nombre del Vendedor"; // Puedes ajustar esto según tus datos
-//   //   header("Location: vendedor.php");
-//   //   exit;
-//   // } else {
-//   //   $error_message = "Credenciales inválidas. Inténtalo nuevamente.";
-//   // }
-  
-// }
+session_start();
 ?>
 
 <!DOCTYPE html>
@@ -42,7 +19,7 @@
 
       <ul>
 
-        <li><a href="index.html">Inicio</a></li>
+        <li><a href="Index.php">Inicio</a></li>
         <li><a href="productos.php">Productos</a></li>
         <li><a href="contacto.html">Contacto</a></li>
         <li><a href="./Vistas/Login.php">Login</a></li>
